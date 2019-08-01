ca187bef6a4790d60e10c50bd078490ccaedd3c5
Omar <omar.abdelbadie1@gmail.com>
2019-08-01 00:22:44 -0700

    passdown props

--- parent-frame/src/App.js
+++ parent-frame/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -13,18 +13,27 @@ console.log(`Hey this is MyWidget: ${MyReactWidget}`)
 
 function App(props) {
   const [myThings, setMyThings] = useState(["burger", "fries"])
+  let [widgetName, setWidgetName] = useState('')
+  
+  useEffect(()=> {setWidgetName(window.prompt("What's the child's name?"))}, [])
+  // console.log(widgetName)
 
   const widgetFunc = word => setMyThings(myThings => [word, ...myThings])
-  console.log(myThings)
+  // const updateWidgetName = event => {setWidgetName(event.target.value); this.forceUpdate()}
+  
   return (
     <div className="App">
       <header className="App-header">
+        {/* <input
+        value={widgetName}
+        onChange={updateWidgetName}
+        placeholder="Pass a property to the child"/> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* This is my count: {count} */}
           These are my things: {myThings.map((thing, idx) => {
             let randColor = Math.floor(Math.random()*16777215).toString(16);
-            console.log(randColor)
+            
             return (<span 
             key={`thing${idx}`} 
             style={{
@@ -34,7 +43,7 @@ function App(props) {
             className="randomThing">{thing}; </span>)
         })}.
         </p>
-        <MyReactWidget name="gooboy" passDownFunc={widgetFunc} />
+        {widgetName && <MyReactWidget name={widgetName} passDownFunc={widgetFunc} />}
         <a
           className="App-link"
           href="https://reactjs.org"
